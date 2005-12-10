ccb2f968cc272892934ff5c6db670016fb080f6b
w.dee <w.dee@9e463c26-7fdd-0310-8662-bb9cb7c2284e>
2005-12-10 15:08:29 +0000

    ReleaserからXP4Archive.hをコピー
    本来は共通のヘッダファイルを参照するように設定するべきだが (Refs #6) 、それはとりあえず後で考えることにする。

---
+++ src/core/base/fs/xp4fs/XP4Archive.h
@@ -0,0 +1,38 @@
+//---------------------------------------------------------------------------
+/*
+	TVP3 ( T Visual Presenter 3 )  A script authoring tool
+	Copyright (C) 2000-2005  W.Dee <dee@kikyou.info> and contributors
+
+	See details of license at "license.txt"
+*/
+//---------------------------------------------------------------------------
+//! @file
+//! @brief XP4 アーカイブファイルシステム
+//---------------------------------------------------------------------------
+
+#ifndef XP4ArchiveH
+#define XP4ArchiveH
+
+
+
+#define TVP_XP4_INDEX_ENCODE_METHOD_MASK 0x03
+#define TVP_XP4_INDEX_ENCODE_RAW      0
+#define TVP_XP4_INDEX_ENCODE_ZLIB     1
+
+#define TVP_XP4_SEGM_ENCODE_METHOD_MASK  0x03
+#define TVP_XP4_SEGM_ENCODE_RAW       0
+#define TVP_XP4_SEGM_ENCODE_ZLIB      1
+
+#define TVP_XP4_FILE_COMPRESSED 0x80	//!< not a real code; only used in tTVPXP4Writer related
+										//!   (do not trust this flag, the actual
+										//!    compression bit is in each segment flags)
+#define TVP_XP4_FILE_EXCLUDED	0x40	//!< not a real code; only used in tTVPXP4Writer related
+#define TVP_XP4_FILE_MARKED		0x20
+#define TVP_XP4_FILE_STATE_MASK	0x0f
+#define TVP_XP4_FILE_STATE_NONE		0
+#define TVP_XP4_FILE_STATE_ADDED	1
+#define TVP_XP4_FILE_STATE_DELETED	2
+#define TVP_XP4_FILE_STATE_MODIFIED	3
+//---------------------------------------------------------------------------
+
+#endif

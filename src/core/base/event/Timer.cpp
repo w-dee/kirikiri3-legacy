4a60a4df45bcd45ed312ed824e60e95c59621817
w.dee <w.dee@9e463c26-7fdd-0310-8662-bb9cb7c2284e>
2006-02-28 13:52:43 +0000

    ひどい、TortoiseMargeにソースをぶっ壊されたので修正。
    TortoiseMargeはRisaのソースの文字コードであるUTF-8に対応していないため。
    
    正確には対応してないではなくて、UTF-8のBOMがないとUTF-8として認識しない。
    ただgccなどはBOMをゴミとして認識してエラーを穿いてしまうため、結局のところTortoiseMargeでUTF-8は使えない。
    
    …ちゃんと svn:mime-type から文字コードをひろって各文字コードに対応、とかなってくれないかなぁ (Tracがそうしているように)

--- src/core/base/event/Timer.cpp
+++ src/core/base/event/Timer.cpp
@@ -8,7 +8,8 @@
 */
 //---------------------------------------------------------------------------
 //! @file
-//! @brief 高精度タイマEの実裁E//---------------------------------------------------------------------------
+//! @brief 高精度タイマーの実装
+//---------------------------------------------------------------------------
 #include "prec.h"
 #include "base/event/Timer.h"
 
@@ -16,12 +17,16 @@ RISSE_DEFINE_SOURCE_ID(3263,62535,57591,17893,921,30607,15180,25430);
 
 /*! @note
 
-	OS の提供する「普通E」タイマEは
-ぁEぁEE解能が低く、使ぁEにならなぁE	(侁E Windowsでは刁E能は50msが限度)、E	刁E能の高いタイマEを汎用皁Eどのように実現するかE難しいところであるが、E	ここではタイミングを制御するスレチEを一つ作Eし、そのスレチE冁Eセマフォ
-	征Eがタイ
-アウトするEと、tRisaTickCount クラスからのチEチEカウントを
-	リファレンスとしてタイマEを実現する、E	これはおおよそOSのコンチEストスイチEの精度に依存するが、E0ms程度の
-	刁E能を提供できるはずである、E	精度につぁEは TickCount.cpp の説明も参Eのこと、E
+	OS の提供する「普通の」タイマーはだいたい分解能が低く、使い物にならない
+	(例: Windowsでは分解能は50msが限度)。
+	分解能の高いタイマーを汎用的にどのように実現するかは難しいところであるが、
+	ここではタイミングを制御するスレッドを一つ作成し、そのスレッド内でセマフォ
+	待ちがタイムアウトするのと、tRisaTickCount クラスからのティックカウントを
+	リファレンスとしてタイマーを実現する。
+	これはおおよそOSのコンテキストスイッチの精度に依存するが、10ms程度の
+	分解能を提供できるはずである。
+	精度については TickCount.cpp の説明も参照のこと。
+
 */
 
 
@@ -30,42 +35,49 @@ RISSE_DEFINE_SOURCE_ID(3263,62535,57591,17893,921,30607,15180,25430);
 //---------------------------------------------------------------------------
 tRisaTimerScheduler::tRisaTimerScheduler()
 {
-	// フィールドE初期匁E	NearestTick = tRisaTickCount::InvalidTickCount;
+	// フィールドの初期化
+	NearestTick = tRisaTickCount::InvalidTickCount;
 	NearestInfoValid = false;
 	NeedRescheduleOnPeriodChange = true;
 
-	// スレチEの実衁E	Run();
+	// スレッドの実行
+	Run();
 }
 //---------------------------------------------------------------------------
 
 
 //---------------------------------------------------------------------------
-//! @brief		チEトラクタ
+//! @brief		デストラクタ
 //---------------------------------------------------------------------------
 tRisaTimerScheduler::~tRisaTimerScheduler()
 {
-	// スレチEの削除
-	Terminate(); // 終亁EラグをたてめE	Event.Signal(); // スレチEをたたき起こす
+	// スレッドの削除
+	Terminate(); // 終了フラグをたてる
+	Event.Signal(); // スレッドをたたき起こす
 }
 //---------------------------------------------------------------------------
 
 
 //---------------------------------------------------------------------------
 //! @brief		consumerを登録する
-//! @param		consumer コールバックを発生させるコンシューマオブジェクチE//! @note		すでにそEconsumerが登録されてるかどぁEなどはチェチEしなぁE//---------------------------------------------------------------------------
+//! @param		consumer コールバックを発生させるコンシューマオブジェクト
+//! @note		すでにそのconsumerが登録されてるかどうかなどはチェックしない
+//---------------------------------------------------------------------------
 void tRisaTimerScheduler::Register(tRisaTimerConsumer * consumer)
 {
 	volatile tRisaCriticalSection::tLocker cs_holder(CS);
 
 	Consumers.push_back(consumer);
-	Reschedule(); // スケジュールし直ぁE}
+	Reschedule(); // スケジュールし直す
+}
 //---------------------------------------------------------------------------
 
 
 //---------------------------------------------------------------------------
 //! @brief		consumerの登録を解除する
-//! @param		consumer コールバックを発生させるコンシューマオブジェクチE//! @note		そEconsumerが見つからなぁE
-合E何もしなぁE//---------------------------------------------------------------------------
+//! @param		consumer コールバックを発生させるコンシューマオブジェクト
+//! @note		そのconsumerが見つからない場合は何もしない
+//---------------------------------------------------------------------------
 void tRisaTimerScheduler::Unregister(tRisaTimerConsumer * consumer)
 {
 	volatile tRisaCriticalSection::tLocker cs_holder(CS);
@@ -74,29 +86,33 @@ void tRisaTimerScheduler::Unregister(tRisaTimerConsumer * consumer)
 	if(i != Consumers.end())
 	{
 		Consumers.erase(i);
-		Reschedule(); // スケジュールし直ぁE	}
+		Reschedule(); // スケジュールし直す
+	}
 }
 //---------------------------------------------------------------------------
 
 
 //---------------------------------------------------------------------------
-//! @brief		スケジュールをやり直ぁE//---------------------------------------------------------------------------
+//! @brief		スケジュールをやり直す
+//---------------------------------------------------------------------------
 void tRisaTimerScheduler::Reschedule()
 {
-	NearestInfoValid = false; // NearestInfo をもぁE度検索するように
-	Event.Signal(); // スレチEをたたき起こす
+	NearestInfoValid = false; // NearestInfo をもう一度検索するように
+	Event.Signal(); // スレッドをたたき起こす
 }
 //---------------------------------------------------------------------------
 
 
 //---------------------------------------------------------------------------
-//! @brief		もっとも近い位置にあるTickをもつConsumerを探ぁE//---------------------------------------------------------------------------
+//! @brief		もっとも近い位置にあるTickをもつConsumerを探す
+//---------------------------------------------------------------------------
 void tRisaTimerScheduler::GetNearestInfo()
 {
 	size_t nearest_index = static_cast<size_t>(-1L);
 	risse_uint64 nearest_tick = tRisaTickCount::InvalidTickCount;
 
-	// もっちも近い位置にある (もっとも値の小さいtickをもつ)Consumerを探ぁE	size_t index = 0;
+	// もっちも近い位置にある (もっとも値の小さいtickをもつ)Consumerを探す
+	size_t index = 0;
 	for(tConsumers::iterator i = Consumers.begin();
 		i != Consumers.end(); i++, index++)
 	{
@@ -120,52 +136,58 @@ void tRisaTimerScheduler::GetNearestInfo()
 
 
 //---------------------------------------------------------------------------
-//! @brief		スレチEのエントリーポインチE//! @return		スレチEの終亁EーチE//---------------------------------------------------------------------------
+//! @brief		スレッドのエントリーポイント
+//! @return		スレッドの終了コード
+//---------------------------------------------------------------------------
 void tRisaTimerScheduler::Execute()
 {
 	while(!ShouldTerminate())
 	{
-		risse_int64 sleep_ms; // どれぐらい sleep すればよいぁE		while(true)
+		risse_int64 sleep_ms; // どれぐらい sleep すればよいか
+		while(true)
 		{
 			risse_uint64 current_tick = tRisaTickCount::instance()->Get();
 
-			// 直近ETickを持つConsumerを探し、何ms後に起きれば良ぁEEかを計算すめE			{
+			// 直近のTickを持つConsumerを探し、何ms後に起きれば良いのかを計算する
+			{
 				volatile tRisaCriticalSection::tLocker cs_holder(CS);
 				if(!NearestInfoValid) GetNearestInfo();
 
 				if(NearestTick == tRisaTickCount::InvalidTickCount)
 				{
 					sleep_ms = -1L;
-					break; // 直近ETickを持つConsumerが屁EぁE				}
+					break; // 直近のTickを持つConsumerが居ない
+				}
 
 				sleep_ms = NearestTick - current_tick;
 				if(sleep_ms <= 0)
 				{
-					// すでに時間をすぎてぁEのでOnPeriodを呼ぶ
-					// OnPeriod 冁E Reschedule を呼ばれても、結局の所
-					// またこのループE先
-に戻って
-					// リスケジュールするのでそうぁE無駁EことをしなぁEぁE
-					// NeedRescheduleOnPeriodChange めEfalse に設定すめE					NeedRescheduleOnPeriodChange = false;
+					// すでに時間をすぎているのでOnPeriodを呼ぶ
+					// OnPeriod 内で Reschedule を呼ばれても、結局の所
+					// またこのループの先頭に戻って
+					// リスケジュールするのでそういう無駄なことをしないように
+					// NeedRescheduleOnPeriodChange を false に設定する
+					NeedRescheduleOnPeriodChange = false;
 					Consumers[NearestIndex]->OnPeriod(NearestTick, current_tick);
 					NeedRescheduleOnPeriodChange = true;
 					NearestInfoValid = false; // NearestInfo を無効に
 				}
 				else
 				{
-					// ま
-時間があるEで sleep する
+					// まだ時間があるので sleep する
 					break;
 				}
 			}
 		}
 
-		// 実際に征E時間を決宁E		// 実際に征E時間は、本当に征Eべき時間よりも少なくてもかまわなぁE		// (そE場合E再スケジュールされるだけなので)
-		// ここでは最長60秒征Eことにする
+		// 実際に待つ時間を決定
+		// 実際に待つ時間は、本当に待つべき時間よりも少なくてもかまわない
+		// (その場合は再スケジュールされるだけなので)
+		// ここでは最長60秒待つことにする
 		if(sleep_ms < 0 || sleep_ms > 60*1000)
 			sleep_ms = 60*1000;
 
-		// 征E
+		// 待つ
 		if(sleep_ms != 0) Event.Wait(static_cast<unsigned long>(sleep_ms));
 	}
 }
@@ -194,7 +216,8 @@ void tRisaTimerScheduler::Execute()
 //---------------------------------------------------------------------------
 tRisaTimerConsumer::tRisaTimerConsumer(tRisaTimerScheduler * owner) : Owner(owner)
 {
-	// フィールドE初期匁E	NextTick = tRisaTickCount::InvalidTickCount;
+	// フィールドの初期化
+	NextTick = tRisaTickCount::InvalidTickCount;
 
 	// スケジューラにコンシューマを登録
 	Owner->Register(this);
@@ -203,7 +226,7 @@ tRisaTimerConsumer::tRisaTimerConsumer(tRisaTimerScheduler * owner) : Owner(owne
 
 
 //---------------------------------------------------------------------------
-//! @brief		チEトラクタ
+//! @brief		デストラクタ
 //---------------------------------------------------------------------------
 tRisaTimerConsumer::~tRisaTimerConsumer()
 {
@@ -214,11 +237,13 @@ tRisaTimerConsumer::~tRisaTimerConsumer()
 
 
 //---------------------------------------------------------------------------
-//! @brief		次にOnPeriodをコールバックすべき絶対tickを設定すめE//! @param		nexttick 絶対TickCount (呼んで欲しくなぁE
-合EtRisaTickCount::InvalidTickCount)
-//! @note		nexttick が現在あるぁEEすでに過去
-った
-合E即座に OnPeriodが呼ばれる、E//! @note		ここでぁE「絶対TickCount」とは tRisaTickCount が返すようなTickCountのことである、E//!				たとえE5秒後にOnPeriodを呼びたいならE、E//!				SetNextTick(tRisaTickCount::instance()->Get() + 5000) とする、E//---------------------------------------------------------------------------
+//! @brief		次にOnPeriodをコールバックすべき絶対tickを設定する
+//! @param		nexttick 絶対TickCount (呼んで欲しくない場合はtRisaTickCount::InvalidTickCount)
+//! @note		nexttick が現在あるいはすでに過去だった場合は即座に OnPeriodが呼ばれる。
+//! @note		ここでいう「絶対TickCount」とは tRisaTickCount が返すようなTickCountのことである。
+//!				たとえば5秒後にOnPeriodを呼びたいならば、
+//!				SetNextTick(tRisaTickCount::instance()->Get() + 5000) とする。
+//---------------------------------------------------------------------------
 void tRisaTimerConsumer::SetNextTick(risse_uint64 nexttick)
 {
 	if(Owner->NeedRescheduleOnPeriodChange)
@@ -226,7 +251,7 @@ void tRisaTimerConsumer::SetNextTick(risse_uint64 nexttick)
 		volatile tRisaCriticalSection::tLocker cs_holder(Owner->CS);
 
 		NextTick = nexttick;
-		Owner->Reschedule(); // スケジュールをやり直すよぁE
+		Owner->Reschedule(); // スケジュールをやり直すように
 	}
 	else
 	{
@@ -258,7 +283,8 @@ void tRisaTimerConsumer::SetNextTick(risse_uint64 nexttick)
 tRisaEventTimerConsumer::tRisaEventTimerConsumer() :
 		tRisaTimerConsumer(tRisaEventTimerScheduler::instance().get())
 {
-	// フィールドE初期匁E	Enabled = false;
+	// フィールドの初期化
+	Enabled = false;
 	Interval = 0;
 	ReferenceTick = tRisaTickCount::InvalidTickCount;
 }
@@ -266,7 +292,7 @@ tRisaEventTimerConsumer::tRisaEventTimerConsumer() :
 
 
 //---------------------------------------------------------------------------
-//! @brief		チEトラクタ
+//! @brief		デストラクタ
 //---------------------------------------------------------------------------
 tRisaEventTimerConsumer::~tRisaEventTimerConsumer()
 {
@@ -275,7 +301,9 @@ tRisaEventTimerConsumer::~tRisaEventTimerConsumer()
 
 
 //---------------------------------------------------------------------------
-//! @brief		有効か無効かを設定すめE//! @param		enabled 有効か無効ぁE//---------------------------------------------------------------------------
+//! @brief		有効か無効かを設定する
+//! @param		enabled 有効か無効か
+//---------------------------------------------------------------------------
 void tRisaEventTimerConsumer::SetEnabled(bool enabled)
 {
 	volatile tRisaCriticalSection::tLocker cs_holder(CS);
@@ -291,7 +319,9 @@ void tRisaEventTimerConsumer::SetEnabled(bool enabled)
 
 
 //---------------------------------------------------------------------------
-//! @brief		タイマE周期を設定すめE//! @param		interval タイマE周朁E//---------------------------------------------------------------------------
+//! @brief		タイマー周期を設定する
+//! @param		interval タイマー周期
+//---------------------------------------------------------------------------
 void tRisaEventTimerConsumer::SetInterval(risse_uint64 interval)
 {
 	volatile tRisaCriticalSection::tLocker cs_holder(CS);
@@ -306,7 +336,7 @@ void tRisaEventTimerConsumer::SetInterval(risse_uint64 interval)
 
 
 //---------------------------------------------------------------------------
-//! @brief		タイマEをリセチEする
+//! @brief		タイマーをリセットする
 //---------------------------------------------------------------------------
 void tRisaEventTimerConsumer::Reset()
 {
@@ -318,29 +348,33 @@ void tRisaEventTimerConsumer::Reset()
 
 
 //---------------------------------------------------------------------------
-//! @brief		タイマE周期をリセチEする
+//! @brief		タイマー周期をリセットする
 //---------------------------------------------------------------------------
 void tRisaEventTimerConsumer::ResetInterval()
 {
-	// 時間原点をこのメソチEが呼ばれた時点に設定すめE
+	// 時間原点をこのメソッドが呼ばれた時点に設定する
+
 	if(Enabled && Interval != tRisaTickCount::InvalidTickCount)
 	{
-		// 有効の場吁E		ReferenceTick = tRisaTickCount::instance()->Get() + Interval;
+		// 有効の場合
+		ReferenceTick = tRisaTickCount::instance()->Get() + Interval;
 		SetNextTick(ReferenceTick);
 	}
 	else
 	{
-		// 無効の場吁E		SetNextTick(tRisaTickCount::InvalidTickCount);
+		// 無効の場合
+		SetNextTick(tRisaTickCount::InvalidTickCount);
 	}
 
-	tRisaEventSystem::instance()->CancelEvents(this); // pending なイベントEすべてキャンセル
+	tRisaEventSystem::instance()->CancelEvents(this); // pending なイベントはすべてキャンセル
 }
 //---------------------------------------------------------------------------
 
 
 //---------------------------------------------------------------------------
-//! @brief		持EされたTickCountに達したときに呼び出されめE//! @param	scheduled_tick GetNextTick() が返しぁETick (本来呼ばれるべき時点の絶対TickCount)
-//! @param	current_tick こE OnPeriod が呼ばれた時点でのtick カウンチE実際に呼ばれた時点での絶対TickCount)
+//! @brief		指定されたTickCountに達したときに呼び出される
+//! @param	scheduled_tick GetNextTick() が返した Tick (本来呼ばれるべき時点の絶対TickCount)
+//! @param	current_tick この OnPeriod が呼ばれた時点でのtick カウント(実際に呼ばれた時点での絶対TickCount)
 //---------------------------------------------------------------------------
 void tRisaEventTimerConsumer::OnPeriod(risse_uint64 scheduled_tick, risse_uint64 current_tick)
 {
@@ -348,7 +382,7 @@ void tRisaEventTimerConsumer::OnPeriod(risse_uint64 scheduled_tick, risse_uint64
 
 	if(Enabled && Interval != tRisaTickCount::InvalidTickCount)
 	{
-		// イベント管琁EスチEにイベントをPostする
+		// イベント管理システムにイベントをPostする
 		tRisaEventSystem::instance()->PostEvent(
 			new tRisaEventInfo(
 				0, // id
@@ -356,7 +390,8 @@ void tRisaEventTimerConsumer::OnPeriod(risse_uint64 scheduled_tick, risse_uint64
 				this // destination
 				) );
 
-		// 次にこEイベントを発生すべぁEtick を設定すめE		ReferenceTick += Interval;
+		// 次にこのイベントを発生すべき tick を設定する
+		ReferenceTick += Interval;
 		SetNextTick(ReferenceTick);
 	}
 	else
@@ -368,14 +403,13 @@ void tRisaEventTimerConsumer::OnPeriod(risse_uint64 scheduled_tick, risse_uint64
 
 
 //---------------------------------------------------------------------------
-//! @brief	イベントが配信されるとぁE//! @param	info イベント情報
+//! @brief	イベントが配信されるとき
+//! @param	info イベント情報
 //---------------------------------------------------------------------------
 void tRisaEventTimerConsumer::OnEvent(tRisaEventInfo * info)
 {
 	if(Enabled && Interval != tRisaTickCount::InvalidTickCount)
-	{
 		OnTimer();
-	}
 }
 //---------------------------------------------------------------------------
 

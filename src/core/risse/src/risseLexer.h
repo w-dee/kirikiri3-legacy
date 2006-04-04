6c1d2c4ea592efeb6b5c9bf2d123596742f845ed
w.dee <w.dee@9e463c26-7fdd-0310-8662-bb9cb7c2284e>
2006-04-04 15:17:06 +0000

    lexer の実装を TJS2 からコピー。
    また、Risse 向けの修正を開始。
    
    lexer の実装は、まだ Risse の他の部分の実装が未熟すぎるために、それらにも手を加えながら実装を行うため、かなり長期にわたる可能性がある。
    
    そのため、コンパイル不可能でも修正途中でも、しばらくの間はコミットを続けることになりそう。

---
+++ src/core/risse/src/risseLexer.h
@@ -0,0 +1,236 @@
+//---------------------------------------------------------------------------
+/*
+	Risse [りせ]
+	 stands for "Risse Is a Sweet Script Engine"
+	Copyright (C) 2000-2006 W.Dee <dee@kikyou.info> and contributors
+
+	See details of license at "license.txt"
+*/
+//---------------------------------------------------------------------------
+//! @file
+//! @brief Risse レキシカル・アナライザ(字句解析機)の実装
+//---------------------------------------------------------------------------
+#include "prec.h"
+
+namespace Risse
+{
+//---------------------------------------------------------------------------
+class tRisseLexer
+{
+public: // public static members
+	static risse_int HexNum(risse_char ch);
+	static risse_int OctNum(risse_char ch);
+	static risse_int DecNum(risse_char ch);
+	static risse_int BinNum(risse_char ch);
+	static risse_int UnescapeBackSlash(risse_char ch);
+
+	//! @brief	コメントをスキップしようとした際の状態
+	enum tSkipCommentResult
+	{
+		scrContinue,	//!< スクリプトはまだ続く
+		scrEnded,		//!< スクリプトは終わった
+		scrNotComment	//!< 実はコメントではなかった
+	};
+
+	static tRisseSkipCommentResult SkipComment(const risse_char * & ptr);
+	static bool SkipSpace(const risse_char * & ptr);
+
+
+private:
+	bool StringMatch(const risse_char *wrd, bool isword);
+
+	//! @brief InternalParseString の戻り値
+	enum tInternalParseStringResult
+	{
+		psrNone, //!< (エラー)
+		psrDelimiter, //!< デリミタにぶつかった
+		psrAmpersand, //!< & にぶつかった ( &式; 形式の埋め込み )
+		psrDollar
+	};
+
+	tInternalParseStringResult
+		InternalParseString(tRisseString &val,
+			risse_char delim, bool embexpmode);
+	bool RisseParseString(tRisseString &val);
+
+public:
+	const risse_char * Ptr; //!< 解析ポインタ
+
+};
+
+//---------------------------------------------------------------------------
+} // namespace Risse
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//---------------------------------------------------------------------------
+/*
+	Risse [りせ]
+	 stands for "Risse Is a Sweet Script Engine"
+	Copyright (C) 2000-2006 W.Dee <dee@kikyou.info> and contributors
+
+	See details of license at "license.txt"
+*/
+//---------------------------------------------------------------------------
+//! @brief Risse レキシカル・アナライザ(字句解析機)の実装
+//---------------------------------------------------------------------------
+#ifndef risseLexH
+#define risseLexH
+
+#include "risseConfig.h"
+#include "risseVariant.h"
+#include <vector>
+#include <deque>
+
+
+extern bool risseEnableDicFuncQuickHack;
+// Defining this enables quick-hack, avoiding the dictionary/array parser
+// memory overflow.
+// This is done with replacing %[ ... ] to function { return %[ ... ]; }()
+// and replacing [ ... ] to function { return [ ... ]; }().
+// These replacing is applied for expression which starts with "%[" or "[", 
+// may cause some sideeffects....
+
+namespace Risse
+{
+//---------------------------------------------------------------------------
+extern risse_int RisseHexNum(risse_char ch) throw();
+extern risse_int RisseOctNum(risse_char ch) throw();
+extern risse_int RisseDecNum(risse_char ch) throw();
+extern risse_int RisseBinNum(risse_char ch) throw();
+
+bool RisseParseString(tRisseVariant &val, const risse_char **ptr);
+bool RisseParseNumber(tRisseVariant &val, const risse_char **ptr);
+void RisseReservedWordsHashAddRef();
+void RisseReservedWordsHashRelease();
+enum tRisseSkipCommentResult
+{ scrContinue, scrEnded, scrNotComment };
+//---------------------------------------------------------------------------
+class tRisseScriptBlock;
+class tRisseLexicalAnalyzer
+{
+public:
+	tRisseLexicalAnalyzer(tRisseScriptBlock *block, const risse_char *script,
+		bool exprmode, bool resneeded);
+	~tRisseLexicalAnalyzer();
+
+private:
+	const risse_char *Current;
+	risse_int PrevPos;
+	risse_int PrevToken;
+	bool First;
+	bool ExprMode;
+	bool ResultNeeded;
+	risse_int NestLevel;
+
+	bool DicFunc; //----- dicfunc quick-hack
+
+	struct tTokenPair
+	{
+		risse_int token;
+		risse_int value;
+
+		tTokenPair(risse_int token, risse_int value)
+		{
+			this->token = token;
+			this->value = value;
+		}
+	};
+
+	std::deque<tTokenPair> RetValDeque;
+
+//	bool BlockBrace;
+
+	bool RegularExpression;
+	bool BareWord;
+
+	enum tEmbeddableExpressionState
+	{	evsStart, evsNextIsStringLiteral, evsNextIsExpression };
+
+	struct tEmbeddableExpressionData
+	{
+		tEmbeddableExpressionState State;
+		risse_int WaitingNestLevel;
+		risse_int WaitingToken;
+		risse_char Delimiter;
+		bool NeedPlus;
+	};
+
+	std::vector<tEmbeddableExpressionData> EmbeddableExpressionDataStack;
+
+
+	tRisseScriptBlock *Block;
+
+	risse_char *Script;
+
+	tRisseSkipCommentResult SkipUntil_endif();
+	tRisseSkipCommentResult ProcessPPStatement();
+
+	risse_int GetToken(risse_int &value);
+
+	risse_int32 ParsePPExpression(const risse_char *start,
+		risse_int n);
+
+	void PreProcess(void);
+
+	std::vector<tRisseVariant *> Values;
+
+	risse_int PutValue(const tRisseVariant &val);
+
+
+	risse_int IfLevel; // @if nesting level
+
+public:
+	const tRisseVariant & GetValue(risse_int idx) const
+	{
+		return *Values[idx];
+	}
+	const risse_char * GetString(risse_int idx) const
+	{
+		return Values[idx]->GetString();
+	}
+
+	void Free(void);
+
+//	void NextBraceIsBlockBrace();
+
+	risse_int GetCurrentPosition();
+
+	risse_int GetNext(risse_int &value);
+
+	void SetStartOfRegExp(void);
+	void SetNextIsBareWord();
+
+};
+//---------------------------------------------------------------------------
+
+} // namespace Risse
+
+#endif
+

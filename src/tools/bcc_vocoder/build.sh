9bcfee3715a41a664ebe929b3114e296d4fb9781
w.dee <w.dee@9e463c26-7fdd-0310-8662-bb9cb7c2284e>
2006-11-11 15:36:12 +0000

    bcc 向け Phase Vocoder の最適化を強化(そこそこよいコード？)

--- src/tools/bcc_vocoder/build.sh
+++ src/tools/bcc_vocoder/build.sh
@@ -5,14 +5,16 @@ cd $current
 current=`pwd`
 
 cd $current
-rm pv_*.nas
+rm -f pv_*.nas
 
 cd $current/default
+rm -f all.s
 make
 echo "linker may fail, but it's ok ..."
 cat all.s | perl ../conv.pl def_ > ../pv_def.nas
 
 cd $current/sse
+rm -f all.s
 make
 echo "linker may fail, but it's ok ..."
 cat all.s | perl ../conv.pl sse_ > ../pv_sse.nas

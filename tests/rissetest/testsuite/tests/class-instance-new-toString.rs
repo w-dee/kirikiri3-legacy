903c94db61cd36bb2df3dd4eb332c8545b320e4e
w.dee <w.dee@9e463c26-7fdd-0310-8662-bb9cb7c2284e>
2007-01-29 12:58:53 +0000

    スペルミス修正

--- tests/rissetest/testsuite/tests/class-instance-new-toString.rs
+++ tests/rissetest/testsuite/tests/class-instance-new-toString.rs
@@ -1,4 +1,4 @@
 var C = Class.new();
-C.toString = function () { return "This is a instance of class C"; };
+C.toString = function () { return "This is an instance of class C"; };
 var i = C.new();
-return "Hey! " + i; //=> "Hey! This is a instance of class C"
+return "Hey! " + i; //=> "Hey! This is an instance of class C"

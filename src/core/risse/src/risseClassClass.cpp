941d659aca874aae57f751b5e13da3782b5547e1
w.dee <w.dee@9e463c26-7fdd-0310-8662-bb9cb7c2284e>
2007-02-18 14:43:23 +0000

    デバッグ用コードを除去

--- src/core/risse/src/risseClassClass.cpp
+++ src/core/risse/src/risseClassClass.cpp
@@ -87,7 +87,6 @@ void tRisseClassClass::RegisterMembers()
 	RISSE_BEGIN_NATIVE_METHOD(ss_include)
 	{
 		// クラスの modules 配列にモジュールを追加する
-		This.DebugDump();
 
 		// modules を取り出す
 		tRisseVariant modules = This.GetPropertyDirect(ss_modules, tRisseOperateFlags::ofInstanceMemberOnly);

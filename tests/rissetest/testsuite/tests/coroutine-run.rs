879b6153b087d8e51f1badecbfc708244a58e66a
w.dee <w.dee@9e463c26-7fdd-0310-8662-bb9cb7c2284e>
2007-07-02 13:20:03 +0000

    追加し忘れていたコルーチン関連のテストケースを追加

---
+++ tests/rissetest/testsuite/tests/coroutine-run.rs
@@ -0,0 +1,38 @@
+class A extends Coroutine
+{
+	function run(arg)
+	{
+		var count = arg;
+		while(true) {
+			var res = "a:" + count + " ";
+			count +=    yield(res);
+		}
+	}
+}
+
+class B extends Coroutine
+{
+	function run(arg)
+	{
+		var count = arg;
+		while(true) {
+			var res = "b:" + count + " ";
+			count +=    yield(res);
+		}
+	}
+}
+
+
+var a = new A();
+var b = new B();
+
+a.resume(0)+
+b.resume(1)+
+a.resume(2)+
+b.resume(3)+
+a.resume(4)+
+b.resume(5)+
+a.resume(6)+
+b.resume(7);
+
+  //=> "a:0 b:1 a:2 b:4 a:6 b:9 a:12 b:16 "

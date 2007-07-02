3cdc0c48106173bbb20fbf46332c36257682d819
w.dee <w.dee@9e463c26-7fdd-0310-8662-bb9cb7c2284e>
2007-07-02 15:20:29 +0000

    配列リテラルのテストケースを追加

---
+++ tests/rissetest/testsuite/tests/array-literal.rs
@@ -0,0 +1,22 @@
+
+var td = [
+ [ 1 , 2 , 3 ],
+ ["a","b","c"],
+ [1.5,2.5,3.5],
+];
+
+
+var res = '';
+for(var y = 0; y < td.length; y++)
+{
+	var row = td[y];
+	for(var x = 0; x < row.length; x++)
+	{
+		res += ':' + row[x];
+	}
+	res += "|";
+}
+
+
+res;
+//=> ":1:2:3|:a:b:c|:1.5:2.5:3.5|"

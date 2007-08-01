fe23d57f48dfed6e1445d2f92c99df6308c6d8be
w.dee <w.dee@9e463c26-7fdd-0310-8662-bb9cb7c2284e>
2007-08-01 15:58:10 +0000

    OSFS 関連テストケースを増強

--- tests/core/tests/filesystem-osfs.rs
+++ tests/core/tests/filesystem-osfs.rs
@@ -16,6 +16,39 @@ stream.dispose();
 assert(data == (octet)"Hello world!\n");
 
 // 大文字と小文字が違うファイルを開こうとしてみる
-var stream = File::open('/tmp/Test.txt');
+try {
+	stream = File::open('/tmp/Test.txt');
+} catch(e) {
+	assert(e.class === IOException);
+}
+
+// サブディレクトリを作る
+File::createDirectory('/tmp/subdir');
+
+// ディレクトリは作られたか？
+assert(File::isDirectory('/tmp/subdir'));
+assert(File::isDirectory('/tmp/subdir/'));
+assert(File::isDirectory('/tmp/subdir//')); // 一応legal
+assert(File::isDirectory('/tmp//subdir//')); // 一応legal
+assert(!File::isFile('/tmp/subdir'));
+
+// そこにファイルを作ってみる
+File::open('/tmp/subdir/test2.txt', File::omWrite).print("Hello world?\n").dispose();
+
+// ファイルは作られたか？
+assert(File::isFile('/tmp/subdir/test2.txt'));
+assert(!File::isDirectory('/tmp/subdir/test2.txt'));
+assert(File::isFile('/tmp/subdir//test2.txt')); // 一応 legal
+
+// もう一個ファイルを作ってみる
+File::open('/tmp/subdir/test3.txt', File::omWrite).print("Hello world!?\n").dispose();
+
+
+// トラバースしてみる
+var files = '';
+File::walkAt('/tmp/subdir') { |name| files += ':' + name }
+p(files);
+
+
 
 //=> ok

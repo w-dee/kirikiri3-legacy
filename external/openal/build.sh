0f5c2f477b68f56120b5a0425202cbffc43de9a5
w.dee <w.dee@9e463c26-7fdd-0310-8662-bb9cb7c2284e>
2007-08-13 04:39:09 +0000

    Windows の場合は Windows ネイティブの OpenAL 実装を使うように。

--- external/openal/build.sh
+++ external/openal/build.sh
@@ -6,5 +6,46 @@ common_configure_options="
 	--prefix=$prefix
 	"
 
+case "`uname -s`" in
+MINGW* | CYGWIN* )
+	custom_build_func=custom_build_windows
+	;;
+*)
+	custom_build_func=custom_build
+	;;
+esac
+
+
+custom_build()
+{
+	current=`pwd`
+
+	cd OpenAL-Sample
+
+	# configure
+	./configure $common_configure_options
+
+	# ビルド
+	make && make install
+
+	cd $current
+}
+
+custom_build_windows()
+{
+	current=`pwd`
+
+	cd OpenAL-Windows
+
+	# .svn ディレクトリが存在する場合、異常な動作をすることがあるので削除
+	rm -rf `find . -name .svn`
+
+	# ビルド
+	make && make install
+
+	cd $current
+}
+
+
 . ../build_common.sh
 

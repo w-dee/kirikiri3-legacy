225d77fe2c9f39af3373b3f8ec3d017ca9faa957
w.dee <w.dee@9e463c26-7fdd-0310-8662-bb9cb7c2284e>
2008-01-01 14:30:07 +0000

    Copyright付け替え祭り '08
    あけましておめでとうございます。今年もよろしくお願い致します。

--- tests/core/rina/rinaNarrowTextProviderNode.h
+++ tests/core/rina/rinaNarrowTextProviderNode.h
@@ -2,7 +2,7 @@
 /*
 	Risa [りさ]      alias 吉里吉里3 [kirikiri-3]
 	 stands for "Risa Is a Stagecraft Architecture"
-	Copyright (C) 2000-2007 W.Dee <dee@kikyou.info> and contributors
+	Copyright (C) 2000-2008 W.Dee <dee@kikyou.info> and contributors
 
 	See details of license at "license.txt"
 

06625913a0aa2242fcb9cd7361c4306abb4e254a
w.dee <w.dee@9e463c26-7fdd-0310-8662-bb9cb7c2284e>
2008-01-18 09:10:17 +0000

    更新領域を受け付けるプロセスノードの基底クラスの実装を追加

---
+++ tests/core/rina/rina1DUpdateReceiver.h
@@ -0,0 +1,46 @@
+//---------------------------------------------------------------------------
+/*
+	Risa [りさ]      alias 吉里吉里3 [kirikiri-3]
+	 stands for "Risa Is a Stagecraft Architecture"
+	Copyright (C) 2000-2008 W.Dee <dee@kikyou.info> and contributors
+
+	See details of license at "license.txt"
+
+	Rina stands for "Rina is an Imaging Network Assembler"
+*/
+//---------------------------------------------------------------------------
+//! @file
+//! @brief 更新を受け付けることができるクラスの基底クラス
+//---------------------------------------------------------------------------
+#ifndef RINA1DUPDATERECEIVER_H
+#define RINA1DUPDATERECEIVER_H
+
+#include "rina1DRegion.h"
+
+namespace Rina {
+//---------------------------------------------------------------------------
+
+
+//---------------------------------------------------------------------------
+//! @brief		テキストの継承可能プロパティ
+//---------------------------------------------------------------------------
+class t1DUpdateReceiver : public tProcessNode, public Risa::tSubmorph<t1DUpdateReceiver>
+{
+public:
+	typedef tProcessNode inherited;
+
+public:
+	//! @brief		内容の更新があったことを伝える(子ノードから呼ばれる)
+	//! @param		area		範囲
+	virtual void NotifyUpdate(const t1DArea & area) {;}
+
+
+};
+//---------------------------------------------------------------------------
+
+
+
+//---------------------------------------------------------------------------
+}
+
+#endif

359627afde6e261b1f23be110f334520e663903a
w.dee <w.dee@9e463c26-7fdd-0310-8662-bb9cb7c2284e>
2008-01-31 15:37:46 +0000

    rinaTest.cpp : ピン操作の間グラフをロックするように

--- tests/core/rina/rinaTest.cpp
+++ tests/core/rina/rinaTest.cpp
@@ -54,92 +54,96 @@ tTester::tTester()
 void tTester::Test()
 {
 	tGraph * graph = new tGraph();
-
 	tWideTextDrawDeviceNode * dd_node = new tWideTextDrawDeviceNode(graph);
-
 	tWideTextProviderNode * provider_node1 = new tWideTextProviderNode(graph);
-	provider_node1->SetCaption(RISSE_WS("abc"));
-	dd_node->InsertInputPinAt(0);
-	dd_node->GetInputPinAt(0)->Connect(provider_node1->GetOutputPinAt(0));
-	Risa::DownCast<tWideTextMixerInputPin*>(dd_node->GetInputPinAt(0))->
-		GetInheritableProperties().SetPosition(1);
-
 	tWideTextProviderNode * provider_node2 = new tWideTextProviderNode(graph);
-	provider_node2->SetCaption(RISSE_WS("def"));
-	dd_node->InsertInputPinAt(1);
-	dd_node->GetInputPinAt(1)->Connect(provider_node2->GetOutputPinAt(0));
-	Risa::DownCast<tWideTextMixerInputPin*>(dd_node->GetInputPinAt(1))->
-		GetInheritableProperties().SetPosition(5);
-
-	wxFprintf(stderr, wxT("%08x\n"), dd_node->GetInputPinAt(0)->GetAgreedType());
-
 	tWideTextMixerNode * mixer_node = new tWideTextMixerNode(graph);
-
 	tWideTextProviderNode * provider_node3 = new tWideTextProviderNode(graph);
-	provider_node3->SetCaption(RISSE_WS("ghi"));
-	mixer_node->InsertInputPinAt(0);
-	mixer_node->GetInputPinAt(0)->Connect(provider_node3->GetOutputPinAt(0));
-	Risa::DownCast<tWideTextMixerInputPin*>(mixer_node->GetInputPinAt(0))->
-		GetInheritableProperties().SetPosition(0);
-
 	tWideTextProviderNode * provider_node4 = new tWideTextProviderNode(graph);
-	provider_node4->SetCaption(RISSE_WS("jkl"));
-	mixer_node->InsertInputPinAt(1);
-	mixer_node->GetInputPinAt(1)->Connect(provider_node4->GetOutputPinAt(0));
-	Risa::DownCast<tWideTextMixerInputPin*>(mixer_node->GetInputPinAt(1))->
-		GetInheritableProperties().SetPosition(4);
-
 	tMultiTextProviderNode * provider_node5 = new tMultiTextProviderNode(graph);
-	provider_node5->SetCaption(RISSE_WS("mno"));
-	mixer_node->InsertInputPinAt(2);
-	mixer_node->GetInputPinAt(2)->Connect(provider_node5->GetOutputPinAt(0));
-	Risa::DownCast<tWideTextMixerInputPin*>(mixer_node->GetInputPinAt(2))->
-		GetInheritableProperties().SetPosition(8);
-
-	dd_node->InsertInputPinAt(2);
-	dd_node->GetInputPinAt(2)->Connect(mixer_node->GetOutputPinAt(0));
-	Risa::DownCast<tWideTextMixerInputPin*>(dd_node->GetInputPinAt(2))->
-		GetInheritableProperties().SetPosition(9);
-
-
 	tNarrowTextProviderNode * narrow_provider_node = new tNarrowTextProviderNode(graph);
-	narrow_provider_node->SetCaption("narrow");
-
 	tNarrowTextToWideTextConverterNode * converter_node1 = new tNarrowTextToWideTextConverterNode(graph);
-	converter_node1->GetInputPinAt(0)->Connect(narrow_provider_node->GetOutputPinAt(0));
-
-	dd_node->InsertInputPinAt(3);
-	dd_node->GetInputPinAt(3)->Connect(converter_node1->GetOutputPinAt(0));
-	Risa::DownCast<tWideTextMixerInputPin*>(dd_node->GetInputPinAt(3))->
-		GetInheritableProperties().SetPosition(21);
-
 	tMultiTextProviderNode * multi_provider_node = new tMultiTextProviderNode(graph);
-	multi_provider_node->SetCaption(RISSE_WS("multi"));
-
 	tNarrowTextToWideTextConverterNode * converter_node2 = new tNarrowTextToWideTextConverterNode(graph);
-	converter_node2->GetInputPinAt(0)->Connect(multi_provider_node->GetOutputPinAt(0));
-
-	dd_node->InsertInputPinAt(4);
-	dd_node->GetInputPinAt(4)->Connect(converter_node2->GetOutputPinAt(0));
-	Risa::DownCast<tWideTextMixerInputPin*>(dd_node->GetInputPinAt(4))->
-		GetInheritableProperties().SetPosition(28);
-
-	dd_node->InsertInputPinAt(5);
-	dd_node->GetInputPinAt(5)->Connect(converter_node1->GetOutputPinAt(0));
-	Risa::DownCast<tWideTextMixerInputPin*>(dd_node->GetInputPinAt(5))->
-		GetInheritableProperties().SetPosition(46);
-
-	dd_node->InsertInputPinAt(6);
-	dd_node->GetInputPinAt(6)->Connect(mixer_node->GetOutputPinAt(0));
-	Risa::DownCast<tWideTextMixerInputPin*>(dd_node->GetInputPinAt(6))->
-		GetInheritableProperties().SetPosition(34);
-
 	tWideTextReverserNode * rev_node = new tWideTextReverserNode(graph);
-	rev_node->GetInputPinAt(0)->Connect(mixer_node->GetOutputPinAt(0));
-	dd_node->InsertInputPinAt(7);
-	dd_node->GetInputPinAt(7)->Connect(rev_node->GetOutputPinAt(0));
-	Risa::DownCast<tWideTextMixerInputPin*>(dd_node->GetInputPinAt(7))->
-		GetInheritableProperties().SetPosition(64);
+
+	// グラフをロックしてから……
+	{
+		Risa::tCriticalSection::tLocker lock(graph->GetCS());
+
+		provider_node1->SetCaption(RISSE_WS("abc"));
+		dd_node->InsertInputPinAt(0);
+		dd_node->GetInputPinAt(0)->Connect(provider_node1->GetOutputPinAt(0));
+		Risa::DownCast<tWideTextMixerInputPin*>(dd_node->GetInputPinAt(0))->
+			GetInheritableProperties().SetPosition(1);
+
+		provider_node2->SetCaption(RISSE_WS("def"));
+		dd_node->InsertInputPinAt(1);
+		dd_node->GetInputPinAt(1)->Connect(provider_node2->GetOutputPinAt(0));
+		Risa::DownCast<tWideTextMixerInputPin*>(dd_node->GetInputPinAt(1))->
+			GetInheritableProperties().SetPosition(5);
+
+		wxFprintf(stderr, wxT("%08x\n"), dd_node->GetInputPinAt(0)->GetAgreedType());
+
+
+		provider_node3->SetCaption(RISSE_WS("ghi"));
+		mixer_node->InsertInputPinAt(0);
+		mixer_node->GetInputPinAt(0)->Connect(provider_node3->GetOutputPinAt(0));
+		Risa::DownCast<tWideTextMixerInputPin*>(mixer_node->GetInputPinAt(0))->
+			GetInheritableProperties().SetPosition(0);
+
+		provider_node4->SetCaption(RISSE_WS("jkl"));
+		mixer_node->InsertInputPinAt(1);
+		mixer_node->GetInputPinAt(1)->Connect(provider_node4->GetOutputPinAt(0));
+		Risa::DownCast<tWideTextMixerInputPin*>(mixer_node->GetInputPinAt(1))->
+			GetInheritableProperties().SetPosition(4);
+
+		provider_node5->SetCaption(RISSE_WS("mno"));
+		mixer_node->InsertInputPinAt(2);
+		mixer_node->GetInputPinAt(2)->Connect(provider_node5->GetOutputPinAt(0));
+		Risa::DownCast<tWideTextMixerInputPin*>(mixer_node->GetInputPinAt(2))->
+			GetInheritableProperties().SetPosition(8);
+
+		dd_node->InsertInputPinAt(2);
+		dd_node->GetInputPinAt(2)->Connect(mixer_node->GetOutputPinAt(0));
+		Risa::DownCast<tWideTextMixerInputPin*>(dd_node->GetInputPinAt(2))->
+			GetInheritableProperties().SetPosition(9);
+
+
+		narrow_provider_node->SetCaption("narrow");
+
+		converter_node1->GetInputPinAt(0)->Connect(narrow_provider_node->GetOutputPinAt(0));
+
+		dd_node->InsertInputPinAt(3);
+		dd_node->GetInputPinAt(3)->Connect(converter_node1->GetOutputPinAt(0));
+		Risa::DownCast<tWideTextMixerInputPin*>(dd_node->GetInputPinAt(3))->
+			GetInheritableProperties().SetPosition(21);
+
+		multi_provider_node->SetCaption(RISSE_WS("multi"));
+
+		converter_node2->GetInputPinAt(0)->Connect(multi_provider_node->GetOutputPinAt(0));
+
+		dd_node->InsertInputPinAt(4);
+		dd_node->GetInputPinAt(4)->Connect(converter_node2->GetOutputPinAt(0));
+		Risa::DownCast<tWideTextMixerInputPin*>(dd_node->GetInputPinAt(4))->
+			GetInheritableProperties().SetPosition(28);
+
+		dd_node->InsertInputPinAt(5);
+		dd_node->GetInputPinAt(5)->Connect(converter_node1->GetOutputPinAt(0));
+		Risa::DownCast<tWideTextMixerInputPin*>(dd_node->GetInputPinAt(5))->
+			GetInheritableProperties().SetPosition(46);
+
+		dd_node->InsertInputPinAt(6);
+		dd_node->GetInputPinAt(6)->Connect(mixer_node->GetOutputPinAt(0));
+		Risa::DownCast<tWideTextMixerInputPin*>(dd_node->GetInputPinAt(6))->
+			GetInheritableProperties().SetPosition(34);
+
+		rev_node->GetInputPinAt(0)->Connect(mixer_node->GetOutputPinAt(0));
+		dd_node->InsertInputPinAt(7);
+		dd_node->GetInputPinAt(7)->Connect(rev_node->GetOutputPinAt(0));
+		Risa::DownCast<tWideTextMixerInputPin*>(dd_node->GetInputPinAt(7))->
+			GetInheritableProperties().SetPosition(64);
+	}
 
 	{
 		// build queue render

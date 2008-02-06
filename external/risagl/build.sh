2bc9cddaf39846e3d685881d3b1feec3de15d6be
w.dee <w.dee@9e463c26-7fdd-0310-8662-bb9cb7c2284e>
2008-02-06 07:52:45 +0000

    risagl/include 直下にヘッダファイル類を置くのではなく risagl/include/risagl にヘッダファイルを置くように
    #include <risagl/pixel_store.hpp> のようにして使いたいので

--- external/risagl/build.sh
+++ external/risagl/build.sh
@@ -10,7 +10,9 @@ custom_build()
 {
 	# とりあえず、src/include を include にコピーする
 	cd $prefix
+	[ -d include ] || mkdir include
 	cp -pR src_temp/include include
+	mv include/include include/risagl
 	# svn ディレクトリは一応削除しておく………
 	rm -rf `find include -name .svn`
 }

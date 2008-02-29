0373b00d5879746899af7f36e25eb5084e857f29
w.dee <w.dee@9e463c26-7fdd-0310-8662-bb9cb7c2284e>
2008-02-29 15:04:31 +0000

    binutils のせいかもしれないが、xiphのソースからビルドしたDLLが「アプリケーションを正しく初期化できませんでした (0xc0000005)。」などといわれ読み込むことができない。
    ほかの libpng などの DLL は正常なので、原因を探ってみるとリンク時のオプションの --image-base=0x10000000 が問題らしい。--enable-auto-image-base だとうまくいくようなので強制的にオプションを置き換えるように。

--- external/libogg/build.sh
+++ external/libogg/build.sh
@@ -2,6 +2,15 @@
 
 prefix=`pwd`
 
+# src_temp にある configure に手を加える
+# --image-base=0x10000000 というオプションが指定されているが
+# --enable-auto-image-base でないとうまくいかない(原因不明、binutilsのバグ？)
+cd src_temp
+cat configure | sed 's|image-base=0x10000000|enable-auto-image-base|' > configure.new
+mv -f configure.new configure
+cd $prefix
+
+
 common_configure_options="
 	--enable-shared --prefix=$prefix
 	"
--- external/libvorbis/build.sh
+++ external/libvorbis/build.sh
@@ -2,6 +2,16 @@
 
 prefix=`pwd`
 
+
+# src_temp にある configure に手を加える
+# --image-base=0x10000000 というオプションが指定されているが
+# --enable-auto-image-base でないとうまくいかない(原因不明、binutilsのバグ？)
+cd src_temp
+cat configure | sed 's|image-base=0x10000000|enable-auto-image-base|' > configure.new
+mv -f configure.new configure
+cd $prefix
+
+
 ogg_dir=$prefix/../libogg
 
 case "`uname -s`" in

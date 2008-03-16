e52cf8e16b58df6c78468b170261fbdf31e61381
w.dee <w.dee@9e463c26-7fdd-0310-8662-bb9cb7c2284e>
2008-03-16 15:34:45 +0000

    boost の assertion failure を Risse の AssertionError 例外として補足するように

--- src/core/risse/src/risseAssert.cpp
+++ src/core/risse/src/risseAssert.cpp
@@ -38,5 +38,35 @@ void Assert(const char * message, const char * filename, int lineno)
 						tString::AsString((risse_int)lineno)));
 }
 //---------------------------------------------------------------------------
-}
+} // namespace Risse
 
+
+
+namespace boost
+{
+using namespace Risse;
+//---------------------------------------------------------------------------
+//! @brief		boost の assertion failure を捕捉する
+//---------------------------------------------------------------------------
+void assertion_failed(char const * expr, char const * function, char const * file, long line)
+{
+	// user defined
+	fflush(stderr);
+	fflush(stdout);
+	fprintf(stderr, "\n\nRisse: Boost: Assertion failed: '%s' failed on function %s at %s line %ld\n",
+			expr, function, file, line);
+
+	fflush(stderr);
+	fflush(stdout);
+
+	tAssertionErrorClass::Throw(
+		tString(RISSE_WS("boost assertion failed: '%1' failed on function '%2' at source %3 line %4"),
+						tString(expr),
+						tString(function),
+						tString(file),
+						tString::AsString((risse_int)line)));
+}
+//---------------------------------------------------------------------------
+
+
+} // namespace boost

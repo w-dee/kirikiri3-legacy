2dc0a5639ff307db89da184c962545a6027c6bd0
w.dee <w.dee@9e463c26-7fdd-0310-8662-bb9cb7c2284e>
2008-10-30 23:02:59 +0000

    ラスタ画像ソースのノードの実装を追加

---
+++ src/core/risa/packages/risa/graphic/rina/rinaImageSource.cpp
@@ -0,0 +1,160 @@
+//---------------------------------------------------------------------------
+/*
+	Risa [りさ]      alias 吉里吉里3 [kirikiri-3]
+	 stands for "Risa Is a Stagecraft Architecture"
+	Copyright (C) 2000-2008 W.Dee <dee@kikyou.info> and contributors
+
+	See details of license at "license.txt"
+
+	Rina stands for "Rina is an Imaging Network Assembler"
+*/
+//---------------------------------------------------------------------------
+//! @file
+//! @brief RINA ラスタ画像用ソース
+//---------------------------------------------------------------------------
+#include "risa/prec.h"
+#include "risa/packages/risa/graphic/rina/rinaImageSource.h"
+#include "risa/packages/risa/graphic/rina/rinaPin.h"
+#include "risa/common/RisaThread.h"
+#include "risse/include/risseNativeBinder.h"
+#include "risse/include/risseObjectClass.h"
+#include "risse/include/risseStaticStrings.h"
+#include "risa/common/RisseEngine.h"
+
+
+namespace Risa {
+RISSE_DEFINE_SOURCE_ID(7573,48746,61484,31356,16289,41410,60513,41447);
+//---------------------------------------------------------------------------
+
+
+
+
+
+//---------------------------------------------------------------------------
+tImageSourceNodeInstance::tImageSourceNodeInstance()
+{
+	InputPinArrayInstance = NULL;
+	OutputPinArrayInstance = NULL;
+	OutputPinInstance = NULL;
+}
+//---------------------------------------------------------------------------
+
+
+//---------------------------------------------------------------------------
+tInputPinArrayInstance & tImageSourceNodeInstance::GetInputPinArrayInstance()
+{
+	return *InputPinArrayInstance;
+}
+//---------------------------------------------------------------------------
+
+
+//---------------------------------------------------------------------------
+tOutputPinArrayInstance & tImageSourceNodeInstance::GetOutputPinArrayInstance()
+{
+	return *OutputPinArrayInstance;
+}
+//---------------------------------------------------------------------------
+
+
+//---------------------------------------------------------------------------
+void tImageSourceNodeInstance::construct()
+{
+	// 入力ピンインスタンスを作成
+	OutputPinInstance =
+		tClassHolder<tImageOutputPinClass>::instance()->GetClass()->
+			Invoke(ss_new).
+		ExpectAndGetObjectInterface<tImageOutputPinInstance>(
+		tClassHolder<tImageOutputPinClass>::instance()->GetClass()
+		);
+	// 入力ピン配列と出力ピン配列を生成
+	InputPinArrayInstance =
+			tClassHolder<tInputPinArrayClass>::instance()->GetClass()->
+				Invoke(ss_new, tVariant(this)).
+			ExpectAndGetObjectInterface<tInputPinArrayInstance>(
+			tClassHolder<tInputPinArrayClass>::instance()->GetClass()
+			);
+	OutputPinArrayInstance =
+			tClassHolder<tOneOutputPinArrayClass>::instance()->GetClass()->
+				Invoke(ss_new, tVariant(this), tVariant(OutputPinInstance)).
+			ExpectAndGetObjectInterface<tOutputPinArrayInstance>(
+			tClassHolder<tOneOutputPinArrayClass>::instance()->GetClass()
+			);
+}
+//---------------------------------------------------------------------------
+
+
+//---------------------------------------------------------------------------
+void tImageSourceNodeInstance::initialize(const tNativeCallInfo &info)
+{
+	volatile tSynchronizer sync(this); // sync
+
+	info.InitializeSuperClass(info.args); // 引数はそのまま渡す
+}
+//---------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+//---------------------------------------------------------------------------
+tImageSourceNodeClass::tImageSourceNodeClass(tScriptEngine * engine) :
+	tClassBase(tSS<'I','m','a','g','e','S','o','u','r','c','e'>(),
+		tClassHolder<tNodeClass>::instance()->GetClass())
+{
+	RegisterMembers();
+}
+//---------------------------------------------------------------------------
+
+
+//---------------------------------------------------------------------------
+void tImageSourceNodeClass::RegisterMembers()
+{
+	// 親クラスの RegisterMembers を呼ぶ
+	inherited::RegisterMembers();
+
+	// クラスに必要なメソッドを登録する
+	// 基本的に ss_construct と ss_initialize は各クラスごとに
+	// 記述すること。たとえ construct の中身が空、あるいは initialize の
+	// 中身が親クラスを呼び出すだけだとしても、記述すること。
+
+	BindFunction(this, ss_ovulate, &tImageSourceNodeClass::ovulate);
+	BindFunction(this, ss_construct, &tImageSourceNodeInstance::construct);
+	BindFunction(this, ss_initialize, &tImageSourceNodeInstance::initialize);
+}
+//---------------------------------------------------------------------------
+
+
+//---------------------------------------------------------------------------
+tVariant tImageSourceNodeClass::ovulate()
+{
+	return tVariant(new tImageSourceNodeInstance());
+}
+//---------------------------------------------------------------------------
+
+
+
+
+
+
+//---------------------------------------------------------------------------
+//! @brief		ImageSourceNode クラスレジストラ
+//---------------------------------------------------------------------------
+template class tClassRegisterer<
+tSS<'r','i','s','a','.','g','r','a','p','h','i','c','.','r','i','n','a'>,
+	tImageSourceNodeClass>;
+//---------------------------------------------------------------------------
+
+
+
+
+
+
+
+//---------------------------------------------------------------------------
+}
---
+++ src/core/risa/packages/risa/graphic/rina/rinaImageSource.h
@@ -0,0 +1,100 @@
+//---------------------------------------------------------------------------
+/*
+	Risa [りさ]      alias 吉里吉里3 [kirikiri-3]
+	 stands for "Risa Is a Stagecraft Architecture"
+	Copyright (C) 2000-2008 W.Dee <dee@kikyou.info> and contributors
+
+	See details of license at "license.txt"
+
+	Rina stands for "Rina is an Imaging Network Assembler"
+*/
+//---------------------------------------------------------------------------
+//! @file
+//! @brief RINA ラスタ画像用ソース
+//---------------------------------------------------------------------------
+#ifndef RINAIMAGESOURCE_H
+#define RINAIMAGESOURCE_H
+
+#include "risa/packages/risa/graphic/rina/rinaIdRegistry.h"
+#include "risa/packages/risa/graphic/rina/rinaQueue.h"
+#include "risa/packages/risa/graphic/rina/rinaTypes.h"
+#include "risa/packages/risa/graphic/rina/rinaPin.h"
+#include "risa/packages/risa/graphic/rina/rinaImageEdge.h"
+
+namespace Risa {
+//---------------------------------------------------------------------------
+
+
+
+//---------------------------------------------------------------------------
+//! @brief		ラスタ画像用ソースノード
+//---------------------------------------------------------------------------
+class tImageSourceNodeInstance : public tNodeInstance
+{
+	typedef tNodeInstance inherited; //!< 親クラス
+
+	tInputPinArrayInstance * InputPinArrayInstance; //!< 入力ピン配列インスタンス
+	tOutputPinArrayInstance * OutputPinArrayInstance; //!< 出力ピン配列インスタンス
+	tImageOutputPinInstance * OutputPinInstance; //!< 出力ピンインスタンス
+
+public:
+	//! @brief		コンストラクタ
+	tImageSourceNodeInstance();
+
+	//! @brief		デストラクタ(おそらく呼ばれない)
+	virtual ~tImageSourceNodeInstance() {}
+
+public: // サブクラスで実装すべき物
+	//! @brief		入力ピンの配列を得る
+	//! @return		入力ピンの配列
+	virtual tInputPinArrayInstance & GetInputPinArrayInstance();
+
+	//! @brief		出力ピンの配列を得る
+	//! @return		出力ピンの配列
+	virtual tOutputPinArrayInstance & GetOutputPinArrayInstance();
+
+public: // Risse用メソッドなど
+	void construct();
+	void initialize(const tNativeCallInfo &info);
+};
+//---------------------------------------------------------------------------
+
+
+
+//---------------------------------------------------------------------------
+//! @brief		"ImageSource" クラス
+//---------------------------------------------------------------------------
+class tImageSourceNodeClass : public tClassBase
+{
+	typedef tClassBase inherited; //!< 親クラスの typedef
+
+public:
+	//! @brief		コンストラクタ
+	//! @param		engine		スクリプトエンジンインスタンス
+	tImageSourceNodeClass(tScriptEngine * engine);
+
+	//! @brief		各メンバをインスタンスに追加する
+	void RegisterMembers();
+
+	//! @brief		newの際の新しいオブジェクトを作成して返す
+	static tVariant ovulate();
+
+public: // Risse 用メソッドなど
+};
+//---------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+//---------------------------------------------------------------------------
+}
+
+#endif
